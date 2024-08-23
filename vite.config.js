83a73e6c3a3e5e9e62dec9d9ff8320bf006b5361
Alexandr <sanyock.perunov@yandex.ru>
2024-08-23 12:24:29 +0500

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@assets': path.resolve(__dirname, 'src/assets'),
-    },
-  },
+  plugins: [],
 });
